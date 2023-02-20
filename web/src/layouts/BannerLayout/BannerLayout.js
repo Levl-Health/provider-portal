a546e6ae7d4ac034f5edc34677762833e1abb031
jjmakes <jmakelyjr@gmail.com>
2023-02-20 17:05:19 -0500

    adding global background color

--- web/src/layouts/BannerLayout/BannerLayout.js
+++ web/src/layouts/BannerLayout/BannerLayout.js
@@ -1,8 +1,10 @@
 import Navbar from 'src/components/Navbar'
+import { Box } from '@chakra-ui/react'
 
 const BannerLayout = ({ children }) => {
   return (
   <>
+  <Box bg="background.100" h="100vh" w="100vw" position="absolute" zIndex="-1" />
     <header>
       <Navbar />
     </header>
